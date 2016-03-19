d6a90e81491053934a9b472bf90bf9a13dc0d5b2
trentduffy <ct_trentduffy@me.com>
2016-03-19 03:07:21 -0400

    inital profle page commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 
-
-
-
 //controller stuff
 var app = angular.module('myApp', ["firebase"]);
-app.controller('myCtrl', function($scope, $firebaseArray) {
+app.controller('myCtrl', function($scope, $firebaseArray, collegesService) {
 
 	var ref = new Firebase("https://trentduffy.firebaseio.com/CollegeTest");
 
@@ -13,5 +10,56 @@ app.controller('myCtrl', function($scope, $firebaseArray) {
 
     $scope.colleges = $firebaseArray(ref);
 
+    $scope.myFunction = function(collegePassed){
+	
+    	collegesService.setCollege(collegePassed);
+
+	 	window.location.href = "colleges.html";
+
+
+    };
+
+
+
+});
+
+
+//controller stuff
+app.controller('myOtherCtrl', function($scope, $firebaseArray, collegesService) {
+
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+
+	var ref = new Firebase("https://trentduffy.firebaseio.com/CollegeTest");
+
+
+    $scope.colleges = $firebaseArray(ref);
+
+
+	console.log("why?");
+	console.log(collegesService.getCollege());
+
+
+});
+
+
+app.service('collegesService', function() {
+
+ var currentCollege;
+
+  var setCollege = function(newCol) {
+     currentCollege =  newCol;
+
+
+  };
+
+  var getCollege = function() {
+  	return this.currentCollege;
+  };
+
+  return {
+    setCollege: setCollege,
+    getCollege: getCollege
+  };
 
 });
