a3b27af84d43deb1a56f1dc06e9050da846f4b93
trentduffy <ct_trentduffy@me.com>
2016-03-18 22:32:41 -0400

    new new

--- scripts.js
+++ scripts.js
@@ -2,27 +2,16 @@
 
 
 
-var ref = new Firebase("https://trentduffy.firebaseio.com/Colleges");
-
-var colleges;
-
-// Attach an asynchronous callback to read the data at our posts reference
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-  colleges = snapshot.val();
-
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
-
 //controller stuff
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+var app = angular.module('myApp', ["firebase"]);
+app.controller('myCtrl', function($scope, $firebaseArray) {
+
+	var ref = new Firebase("https://trentduffy.firebaseio.com/CollegeTest");
+
     $scope.firstName = "John";
     $scope.lastName = "Doe";
-    $scope.unis = colleges;
+
+    $scope.colleges = $firebaseArray(ref);
+
+
 });
-
-
